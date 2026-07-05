2573e521e5fe2d2df5c54dcd39b5bac90c5870e1
Rajnish018 <197075148+Rajnish018@users.noreply.github.com>
2026-07-05 16:29:04 +0530

    update backend url

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -2,7 +2,8 @@ import { Platform } from "react-native";
 
 // export const API_URL=Platform.OS==='android'?'http://10.48.102.167:3000':'http://10.48.102.167:3000'
 // export const API_URL = Platform.OS === 'android' ? 'http://10.189.123.167:3000' : 'http://10.189.123.167:3001'
-export const API_URL = "http://10.151.67.167:3000"
+export const API_URL = "https://msgly-real-chatting-mobile-app.onrender.com";
+// "http://10.151.67.167:3000"
   // process.env.EXPO_PUBLIC_API_URL ||
   // "https://msgly-real-chatting-mobile-app.onrender.com";
 console.log(API_URL)
